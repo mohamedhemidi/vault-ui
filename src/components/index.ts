0c0bff21c16d90adb219efef33affb314c9194ae
Mohamed <m.hamidi571@gmail.com>
2023-12-09 23:37:11 +0100

    Migration to rollup.js

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { Button } from "./Button";
+export { TextField } from "./Inputs/TextField";
+export { TextArea } from "./Inputs/TextArea";
+export { MultiSelect } from "./Select";
+export { LazyLoadImage } from "./LazyLoadImage";
