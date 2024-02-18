26b2505516ae3ba12ffc82b9d30b730b576e37c4
Mohamed <m.hamidi571@gmail.com>
2024-02-18 23:30:53 +0100

    Toast Component Added

---
+++ src/components/Toast/Toast.tsx
@@ -0,0 +1,70 @@
+import React, { useEffect } from "react";
+import styles from "./styles.module.css";
+import variantIcon from "./toastIcon";
+
+type ToastT = {
+  variant?: "success" | "error" | "warning" | "info";
+  active: boolean;
+  timer?: number;
+  title?: string;
+  message?: string;
+  close: () => void;
+};
+const toastTitle = {
+  success: "Success",
+  error: "Error",
+  warning: "Warning",
+  info: "Info",
+};
+
+const Toast = ({
+  active,
+  close,
+  timer = 3,
+  variant = "success",
+  message,
+  title,
+}: ToastT) => {
+  useEffect(() => {
+    const functionId = setTimeout(() => close(), timer * 1000);
+    return () => clearTimeout(functionId);
+  }, [active, timer, close]);
+
+  return (
+    <div className={styles.notification}>
+      <div className={`${styles.toast} ${active ? styles.active : ""}`}>
+        <div className={styles.toast_content}>
+          <div
+            className={`${styles.icons} ${styles.toast_icon} ${styles[variant]}`}
+          >
+            {variantIcon[variant]}
+          </div>
+
+          <div className={styles.message}>
+            <span className={`${styles.text} ${styles.text_1}`}>
+              {title ? title : toastTitle[variant]}
+            </span>
+            <span className={`${styles.text} ${styles.text_2}`}>{message}</span>
+          </div>
+        </div>
+        <div className={`${styles.icons} ${styles.close_icon}`} onClick={close}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24"
+            viewBox="0 -960 960 960"
+            width="24"
+          >
+            <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+          </svg>
+        </div>
+        <div
+          className={`${styles.progress} ${styles[variant]} ${
+            active ? styles.active : ""
+          }`}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Toast;
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { TextField } from "./Inputs/TextField";
 export { TextArea } from "./Inputs/TextArea";
 export { MultiSelect } from "./Select";
 export { LazyLoadImage } from "./LazyLoadImage";
+export { Toast } from "./Toast";
